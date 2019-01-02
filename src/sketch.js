b415adfa8750b0a68576a97ee6919c5a304c17cd
Chris Neale <chris@komododigital.co.uk>
2019-01-02 08:27:54 +0000

    First example

---
+++ src/sketch.js
@@ -0,0 +1,29 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: "a4",
+  pixelsPerInch: 300,
+  units: "mm"
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "pink";
+    context.fillRect(0, 0, width, height);
+
+    context.strokeStyle = "black";
+    context.beginPath();
+    context.arc(
+      width * 0.5,
+      height * 0.5,
+      width * 0.25,
+      height * 0.25,
+      0,
+      Math.PI * 2,
+      false
+    );
+    context.stroke();
+  };
+};
+
+canvasSketch(sketch, settings);
