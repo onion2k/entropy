13a09569147977005b97437ac93977b122cefd8c
Chris Neale <chris@komododigital.co.uk>
2019-01-03 09:22:47 +0000

    Noisy circles

--- src/sketch.js
+++ src/sketch.js
@@ -1,4 +1,7 @@
 import canvasSketch from "canvas-sketch";
+const tooloud = require("../node_modules/tooloud/dist/tooloud.min.js");
+
+tooloud.Perlin.setSeed(Math.floor(Math.random() * 10000));
 
 const settings = {
   dimensions: "a4",
@@ -11,8 +14,8 @@ const sketch = () => {
     context.fillStyle = "#f5f2e4";
     context.fillRect(0, 0, width, height);
 
-    let h = 10;
-    let v = 16;
+    let h = 6 * 10;
+    let v = 6 * (10 * (height / width));
 
     let rx = width / h;
     let ry = height / v;
@@ -23,20 +26,24 @@ const sketch = () => {
       let _x = x % h;
       let _y = Math.floor(x / h);
 
-      const g = Math.random() * 255;
-      context.strokeStyle = `rgb(${g},${g},${g})`;
+      const n = tooloud.Perlin.noise((5 * _x) / h, (5 * _y) / v, 0);
+
+      const r = Math.floor(255 * n);
+      const g = Math.floor(255 * n);
+      const b = Math.floor(255 * n);
+
+      context.fillStyle = `rgb(${r},${g},${b})`;
       context.beginPath();
       context.arc(
-        _x * rx + rx * 0.5 + (Math.random() * 6 - 3),
-        _y * ry + ry * 0.5 + (Math.random() * 6 - 3),
-        rx * 0.75 + (Math.random() * 6 - 3),
-        rx * 0.75 + (Math.random() * 6 - 3),
+        _x * rx + rx * 0.5,
+        _y * ry + ry * 0.5,
+        rx * 0.5,
         0,
         Math.PI * 2,
         false
       );
 
-      context.stroke();
+      context.fill();
     }
   };
 };
