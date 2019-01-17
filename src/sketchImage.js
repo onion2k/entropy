dd0a3427fb5bede4c2c98e77d50bb4f09f079a5d
Chris Neale <chris@komododigital.co.uk>
2019-01-17 09:03:05 +0000

    Async constructor for sketchImage

---
+++ src/sketchImage.js
@@ -0,0 +1,61 @@
+const loadAsset = require("load-asset");
+export default class sketchImage {
+  constructor(url) {
+    this.img = null;
+    this._img = null;
+    this._width = null;
+    this._height = null;
+    if (url) {
+      return (async () => {
+        await this.load(url);
+        console.log(this.img);
+        return this; // when done
+      })();
+    } else {
+      return this;
+    }
+  }
+  async load(url) {
+    this.img = await loadAsset({ url: url, type: "image" });
+    this._img = this.img;
+    this._width = this.img.width;
+    this._height = this.img.height;
+    return this;
+  }
+  get width() {
+    if (this.img === null) return false;
+    return this.img.width;
+  }
+  get height() {
+    if (this.img === null) return false;
+    return this.img.height;
+  }
+  scale(x, y, maintainAspectRatio) {
+    let pixelCanvas = document.createElement("canvas");
+
+    let _x = x;
+    let _y =
+      maintainAspectRatio === true
+        ? Math.floor(x * (this.height / this.width))
+        : y;
+
+    pixelCanvas.width = _x;
+    pixelCanvas.height = _y;
+
+    const pixelCanvasContext = pixelCanvas.getContext("2d");
+    pixelCanvasContext.imageSmoothingEnabled = false;
+    pixelCanvasContext.drawImage(
+      this._img,
+      0,
+      0,
+      this.img.width,
+      this.img.height,
+      0,
+      0,
+      pixelCanvas.width,
+      pixelCanvas.height
+    );
+
+    this.img = pixelCanvas;
+  }
+}
--- src/svg_loader.js
+++ src/svg_loader.js
@@ -1,5 +1,5 @@
 import canvasSketch from "canvas-sketch";
-const loadAsset = require("load-asset");
+import sketchImage from "./sketchImage";
 
 const settings = {
   dimensions: [800, 800],
@@ -8,60 +8,8 @@ const settings = {
   fps: 60
 };
 
-class imageSketch {
-  constructor() {
-    this.img = null;
-    this._img = null;
-    this._width = null;
-    this._height = null;
-  }
-  async load(url) {
-    this.img = await loadAsset({ url: url, type: "image" });
-    this._img = this.img;
-    this._width = this.img.width;
-    this._height = this.img.height;
-  }
-  get width() {
-    if (this.img === null) return false;
-    return this.img.width;
-  }
-  get height() {
-    if (this.img === null) return false;
-    return this.img.height;
-  }
-  scale(x, y, maintainAspectRatio) {
-    let pixelCanvas = document.createElement("canvas");
-
-    let _x = x;
-    let _y =
-      maintainAspectRatio === true
-        ? Math.floor(x * (this.height / this.width))
-        : y;
-
-    pixelCanvas.width = _x;
-    pixelCanvas.height = _y;
-
-    const pixelCanvasContext = pixelCanvas.getContext("2d");
-    pixelCanvasContext.imageSmoothingEnabled = false;
-    pixelCanvasContext.drawImage(
-      this._img,
-      0,
-      0,
-      this.img.width,
-      this.img.height,
-      0,
-      0,
-      pixelCanvas.width,
-      pixelCanvas.height
-    );
-
-    this.img = pixelCanvas;
-  }
-}
-
 canvasSketch(async ({ update }) => {
-  const image = new imageSketch();
-  await image.load("assets/wine.svg");
+  const image = await new sketchImage("assets/wine.svg");
 
   return ({ context, width, height, time }) => {
     image.scale(50, 50, true);
