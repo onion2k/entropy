36401ef6456f67bd4f5a28f075794d6b85e5e967
Chris Neale <chris@komododigital.co.uk>
2019-01-18 13:29:59 +0000

    Wave effect

--- src/pixel_motion.js
+++ src/pixel_motion.js
@@ -8,6 +8,10 @@ const settings = {
   fps: 60
 };
 
+function inout(t) {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+}
+
 canvasSketch(async ({ update }) => {
   const image = await new sketchImage("assets/wine.svg");
   let x = 50;
@@ -16,7 +20,7 @@ canvasSketch(async ({ update }) => {
   image.scale(x, y, true);
   const pixels = image.data;
 
-  return ({ context, width, height, time }) => {
+  return ({ context, width, height, time, frame }) => {
     context.fillStyle = "#fff";
     context.fillRect(0, 0, width, height);
     for (let counter = 0; counter < i; counter++) {
@@ -26,11 +30,22 @@ canvasSketch(async ({ update }) => {
 
       context.fillStyle = `hsl(0,100%,${50 * Math.floor(n)}%)`;
 
-      const pX = _x * (width / x) + x * 0.5 + (Math.cos(x + time) * 20.0 - 10);
-      const pY = _y * (height / y) + y * 0.5 + (Math.cos(y + time) * 20.0 - 10);
+      // console.log(time, time / x);
+
+      const pX = _x * (width / (x + 0.5)) + x * 0.25;
+      const pY = _y * (height / (y + 0.5)) + y * 0.25;
 
       context.beginPath();
-      context.arc(pX, pY, 5, 0, Math.PI * 2, true);
+      context.arc(
+        pX,
+        pY,
+        5 +
+          Math.sin(frame / 18 + _x * 0.1) * 2 +
+          Math.cos(frame / 18 + _y * 0.1) * 2,
+        0,
+        Math.PI * 2,
+        true
+      );
       context.fill();
     }
   };
